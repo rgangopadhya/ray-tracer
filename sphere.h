c93a57063c5c949f5fe4aa68de6ccd2e104211f5
Raja Gangopadhya <raja.gangopadhya@remix.com>
2021-02-19 19:53:22 -0800

    Metal

--- sphere.h
+++ sphere.h
@@ -7,14 +7,24 @@
 class sphere : public hittable {
     public:
         sphere() {}
-        sphere(point3 cen, double r) : center(cen), radius(r) {}
+        sphere(point3 cen, double r, shared_ptr<material> m) : center(cen), radius(r), mat_ptr(m) {}
         virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
     public:
         point3 center;
         double radius;
+        shared_ptr<material> mat_ptr;
 };
 
 bool sphere::hit(const ray &r, double t_min, double t_max, hit_record &rec) const {
+    /*
+     * Determines if, given a range of parameters (t_min, t_max),
+     * there is a solution for the ray r. If there is, mutate the hit_record rec
+     * with the solution for t and the point that was hit.
+     * also, calculate the normal from the point on the sphere that was hit,
+     * making sure to orient it so that it opposes the ray direction (?)
+     * With the material, we need to pass that in so we know how to handle the
+     * reflection of the hit
+     */
     vec3 oc = r.origin() - center;
     auto a = r.direction().length_squared();
     auto half_b = dot(r.direction(), oc);
@@ -34,6 +44,7 @@ bool sphere::hit(const ray &r, double t_min, double t_max, hit_record &rec) cons
     rec.p = r.at(root);
     vec3 outward_normal = (rec.p - center) / radius;
     rec.set_face_normal(r, outward_normal);
+    rec.mat_ptr = mat_ptr;
 
     return true;
 }
